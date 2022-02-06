b1e8a2e6f2f923c7e5c6ddacd4d0c954319886e2
vdashuk <donvov1990@gmail.com>
2022-02-06 14:53:56 +0200

    Moves the markup to the Toggle component.

---
+++ src/components/Toggle/Toggle.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="filter-toolbar toggle">
+        <input type="checkbox" id="switch" class="switch"/>
+        <label for="switch" class="label-day">Day</label>
+        <label for="switch" class="label-month">Month</label>
+    </div>
+</template>
