c3f6a089c996c0cf2e32aa13a5e09bdb3241b3c2
volodymyrDashuk <donvov1990@gmail.com>
2022-04-18 22:17:03 +0300

    Changes dynamically title of the page

--- src/router/index.js
+++ src/router/index.js
@@ -4,43 +4,49 @@ const routes = [
     {
         path: '/login',
         name: 'Login',
-        meta: {layout: 'Auth', isAuth: false},
+        meta: {layout: 'Auth', isAuth: false, title: 'Tim - Login'},
         component: () => import('../views/Login/Login.vue')
     },
     {
         path: '/registration',
         name: 'Registration',
-        meta: {layout: 'Auth', isAuth: false},
+        meta: {layout: 'Auth', isAuth: false, title: 'Tim - Registration'},
         component: () => import('../views/Registration/Registration.vue')
+    },
+    {
+        path: '/forgot-password',
+        name: 'forgotPassword',
+        meta: {layout: 'Auth', isAuth: false, title: 'Tim - Forgot Password'},
+        component: () => import('../views/ForgotPassword/ForgotPassword.vue')
     },
     {
         path: '/',
         name: 'Dashboard',
-        meta: {layout: 'Main', isAuth: true},
+        meta: {layout: 'Main', isAuth: true, title: 'Tim - Dashboard'},
         component: () => import('../views/Dashboard/Dashboard.vue')
     },
     {
         path: '/project',
         name: 'Project',
-        meta: {layout: 'Main', isAuth: true},
+        meta: {layout: 'Main', isAuth: true, title: 'Tim - Project'},
         component: () => import('../views/Project/Project.vue')
     },
     {
         path: '/activity',
         name: 'Activity',
-        meta: {layout: 'Main', isAuth: true},
+        meta: {layout: 'Main', isAuth: true, title: 'Tim - Activity'},
         component: () => import('../views/Activity/Activity.vue')
     },
     {
         path: '/timesheet',
         name: 'Timesheet',
-        meta: {layout: 'Main', isAuth: true},
+        meta: {layout: 'Main', isAuth: true, title: 'Tim - Timesheet'},
         component: () => import('../views/Timesheet/Timesheet.vue')
-    }
-    ,
+    },
     {
         path: '/:catchAll(.*)',
         name: 'noRoute',
+        meta: {title: 'Tim - Page not found'},
         component: () => import('../views/NoRoute/NoRoute.vue')
     }
 ]
@@ -57,6 +63,7 @@ router.beforeEach((to, from, next) => {
     if (requireAuth && !user) {
         next('/login')
     } else {
+        document.title = `${to.meta.title}`;
         next()
     }
 })
